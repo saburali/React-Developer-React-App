b0826d806e1ab829cb7057877f41911a42dc47de
Abdus Sabur <saburali7264@gmail.com>
2021-10-25 13:45:51 +0600

    Navbar & Front Title Design + Responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './header.css';
-import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Container, Nav, Navbar} from "react-bootstrap";
 
 const Header = () => {
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar collapseOnSelect expand="lg" bg="white" variant="light">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    {/*<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>*/}
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
+                        <Nav className="mx-auto">
                             <NavLink className="nav-link" to="/home">Home</NavLink>
                             <NavLink className="nav-link" to="/question"> Question </NavLink>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                        <Nav>
                             <NavLink className="nav-link" to="/services"> Services </NavLink>
                             <NavLink className="nav-link" to="/about-us">About Us</NavLink>
                             <Nav.Link eventKey={2} to="/contact"> Contact </Nav.Link>
