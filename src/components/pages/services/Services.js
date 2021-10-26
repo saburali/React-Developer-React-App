3a63b4480880206c13ba580bb26ebe41539149c8
Abdus Sabur <saburali7264@gmail.com>
2021-10-26 20:47:29 +0600

    Services Page okk

--- src/components/pages/services/Services.js
+++ src/components/pages/services/Services.js
@@ -4,6 +4,7 @@ import Speciality from "../../speciality/Speciality";
 import {Row, Tabs} from "react-bootstrap";
 import {Tab} from "bootstrap";
 import Rules from "../../rules/Rules";
+import {AiFillStar, FiEdit3} from "react-icons/all";
 
 const Services = () => {
 
@@ -24,9 +25,9 @@ const Services = () => {
     }, [])
 
     return (
-        <div className="container">
-            <Tabs defaultActiveKey="special" id="uncontrolled-tab-example" className="mb-3">
-                <Tab eventKey="special" title="special">
+        <div className="container service">
+            <Tabs defaultActiveKey="special" id="uncontrolled-tab-example" className="mb-5">
+                <Tab eventKey="special" title={<AiFillStar/>}>
                     <Row xs={1} md={2} lg={4} className="g-4">
                         {
                             speciality.map(special =>
@@ -38,7 +39,7 @@ const Services = () => {
                         }
                     </Row>
                 </Tab>
-                <Tab eventKey="rules" title="rules">
+                <Tab eventKey="rules" title={<FiEdit3/>}>
                     <Row xs={1} md={2} lg={2} className="g-4">
                         {
                             rules.map(rule =>
